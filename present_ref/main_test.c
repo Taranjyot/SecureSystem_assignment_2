79238089862360835fe700c435558cb7d40f8db1
Taranjyot <jyotlive91@gmail.com>
2022-04-08 12:50:34 +0100

    Adds main_test code

--- present_ref/main_test.c
+++ present_ref/main_test.c
@@ -17,16 +17,28 @@
 static uint8_t pt[CRYPTO_IN_SIZE] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 static uint8_t key[CRYPTO_KEY_SIZE] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00} ;
 
+//  45 84 22 7B 38 C1 79 55
 int main()
 {
     uint8_t b = 0;
     uint64_t begin = 0, end = 0, duration;
     int c = -1;
 
+
+
+
     printf("%c", pt[0]);
     crypto_func(pt, key);
+
+    for(uint8_t i=0;i<CRYPTO_IN_SIZE;i++) {
+        printf("%hhx", pt[i]);
+        printf(", ");
+    }
+
     printf("Hello world \n");
     printf("%p", (void*) pt);
     printf("\n");
+
+
 }
 
